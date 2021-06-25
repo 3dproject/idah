c4154e704cb4d15dc9a2c05fe988389980b60746
yaminipriyakodeboyina <yaminipriyakodeboyina@gmail.com>
2021-06-25 01:02:16 -0400

    code

---
+++ assests/jscode/jsscript.js
@@ -0,0 +1,8 @@
+$(function(){
+    $(".project-info").on("click",function(){
+        window.location = "../about/about.html/#project-info-div";
+    });
+    $(".contact-info").on("click",function(){
+        window.location = "../about/about.html/#contact-info-div";
+    });
+})

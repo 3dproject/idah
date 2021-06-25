db314fb7908f68ef3bc54e064f4b1ba858f1e0b5
yaminipriyakodeboyina <yaminipriyakodeboyina@gmail.com>
2021-06-25 01:59:07 -0400

    new code7

--- assests/jscode/jsscript.js
+++ assests/jscode/jsscript.js
@@ -3,7 +3,8 @@ $(function(){
         // document.location.href  = "{{site.baseurl}}/about/about.html/#project-info-div";
         // window.location.assign("{{site.baseurl}}/about/about.html/#project-info-div");
         // var l={{site.baseurl}}
-        $(location).attr('href',"{{site.baseurl}}/about/about.html");
+        $(location).attr('href',"{{site.baseurl}}/about/about.html/");
+        document.getElementById('project-info-div').focus();
     });
     $(".contact-info").on("click",function(){
         window.location.href = "{{site.baseurl}}/about/about.html/#contact-info-div";

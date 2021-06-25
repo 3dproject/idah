84b367aae82a6028a1e2c9f766de010a2b23f280
yaminipriyakodeboyina <yaminipriyakodeboyina@gmail.com>
2021-06-25 02:27:03 -0400

    new code8

--- assests/jscode/jsscript.js
+++ assests/jscode/jsscript.js
@@ -1,12 +1,14 @@
 $(function(){
-    $(".project-info").on("click",function(){
-        window.location.href  = "{{site.baseurl}}/about/about.html";
+    $("#project-info").on("click",function(){
+        window.location.href  = window.location.origin+"/about/about.html/#project-info-div";
         // window.location.assign("{{site.baseurl}}/about/about.html/#project-info-div");
         // var l={{site.baseurl}}
-        document.getElementById('project-info-div').focus();
+        // document.getElementById('project-info-div').focus();
     });
-    $(".contact-info").on("click",function(){
-        window.location.href = "{{site.baseurl}}/about/about.html";
+    $("#contact-info").on("click",function(){
+        // window.location.href = "{{site.baseurl}}/about/about.html/#project-info-div";
+        var b= window.location.origin;
+        window.location.assign(b+"/about/about.html");
         document.getElementById('contact-info-div').focus();
     });
 })

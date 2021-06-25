3af3b711a1a6fe6ad5fd89238c01fd7717993ac1
yaminipriyakodeboyina <yaminipriyakodeboyina@gmail.com>
2021-06-25 02:07:27 -0400

    new code8

--- assests/jscode/jsscript.js
+++ assests/jscode/jsscript.js
@@ -1,6 +1,6 @@
 $(function(){
     $(".project-info").on("click",function(){
-        document.location.href  = "{{site.baseurl}}/about/about.html";
+        window.location.href  = "{{site.baseurl}}/about/about.html";
         // window.location.assign("{{site.baseurl}}/about/about.html/#project-info-div");
         // var l={{site.baseurl}}
         document.getElementById('project-info-div').focus();
